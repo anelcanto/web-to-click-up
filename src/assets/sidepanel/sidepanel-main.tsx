647ddbbf09152f7956c520229045ca18e9693846
Anel Canto <ac@anelcanto.com>
2024-12-27 16:08:43 -0700

    Remove unused files

--- src/assets/sidepanel/sidepanel-main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SidePanel from './SidePanel';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <SidePanel />
-    </React.StrictMode>
-);
